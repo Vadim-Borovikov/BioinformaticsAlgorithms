fd001c0ae2ff0a5724572b104ca0d64cabd93bc0
Vadim Borovikov <vadim.borovikov@gmail.com>
2017-11-30 10:54:24 +0300

    StringTaskSolver implemented

---
+++ BioinformaticsAlgorithms/StringTaskSolver.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BioinformaticsAlgorithms
+{
+    public class StringTaskSolver : ITasksSolver
+    {
+        public int PatternCount(string text, string pattern)
+        {
+            int result = 0;
+            for (int i = 0; i <= (text.Length - pattern.Length); ++i)
+            {
+                if (text.Substring(i, pattern.Length) == pattern)
+                {
+                    ++result;
+                }
+            }
+            return result;
+        }
+
+        public IEnumerable<string> FrequentWords(string text, int k)
+        {
+            var amounts = new int[text.Length - k + 1];
+            for (int i = 0; i <= (text.Length - k); ++i)
+            {
+                string pattern = text.Substring(i, k);
+                amounts[i] = PatternCount(text, pattern);
+            }
+            int maxAmount = amounts.Max();
+
+            var frequentWords = new HashSet<string>();
+            for (int i = 0; i <= (text.Length - k); ++i)
+            {
+                if (amounts[i] == maxAmount)
+                {
+                    frequentWords.Add(text.Substring(i, k));
+                }
+            }
+            return frequentWords;
+        }
+    }
+}
---
+++ BioinformaticsAlgorithmsTests/StringTaskSolverTests.cs
@@ -0,0 +1,25 @@
+﻿using BioinformaticsAlgorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BioinformaticsAlgorithmsTests
+{
+    [TestClass]
+    public class StringTaskSolverTests
+    {
+        [TestMethod]
+        public void PatternCountTest()
+        {
+            var solver = new StringTaskSolver();
+            var tests = new SolversTests(solver);
+            tests.PatternCountTest();
+        }
+
+        [TestMethod]
+        public void FrequentWordsTest()
+        {
+            var solver = new StringTaskSolver();
+            var tests = new SolversTests(solver);
+            tests.FrequentWordsTest();
+        }
+    }
+}
